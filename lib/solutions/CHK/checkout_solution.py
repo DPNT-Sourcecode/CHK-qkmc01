446b889fbeb213788ff81e06f5fa84e8832681e2
sbx_user1051 <sbx_user1051@169.254.75.93>
2019-11-17 19:23:56 +0000

    Sun Nov 17 19:23:56 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,16 +19,15 @@ def checkout(skus):
         running_total += price_map[item]
 
     num_product_a = skus.count('A')
-    num_a_discounts = round(num_product_a / 3 if num_product_a >= 3 else 0)
+    num_a_discounts = int(num_product_a / 3 if num_product_a >= 3 else 0)
     a_discount = (num_a_discounts * 20)
 
-    breakpoint()
-
     num_product_b = skus.count('B')
-    num_b_discounts = round(num_product_b / 2 if num_product_b >= 2 else 0)
+    num_b_discounts = int(num_product_b / 2 if num_product_b >= 2 else 0)
     b_discount = (num_b_discounts * 15)
 
     return running_total - a_discount - b_discount
+
 
 
 
