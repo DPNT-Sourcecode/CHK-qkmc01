3c607c55251e19d4b9d487eb96332d616008bc91
sbx_user1051 <sbx_user1051@169.254.75.93>
2019-11-17 21:59:11 +0000

    Sun Nov 17 21:59:11 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -8,17 +8,9 @@ def test_checkout():
 def test_checkout_multiples():
     assert checkout_solution.checkout('ABCDBA') == 180
 
-def test_checkout_discount_times_1():
-    assert checkout_solution.checkout('AAA') == 130
-
-def test_checkout_discount_plus_1():
-    assert checkout_solution.checkout('AAAA') == 180
 
 def test_checkout_three_Bs():
     assert checkout_solution.checkout('BBB') == 75
-
-def test_checkout_discount_times_2():
-    assert checkout_solution.checkout('AAAAAA') == 250
 
 def test_checkout_discount_b_times_1():
     assert checkout_solution.checkout('BB') == 45
@@ -29,24 +21,20 @@ def test_checkout_lower_case_rejected():
 def test_bad_chars_rejected():
     assert checkout_solution.checkout('-') == -1
 
-def test_five_As_for_200():
-    assert checkout_solution.checkout('AAAAA') == 200
 
-def test_eight_As_for_200():
-    assert checkout_solution.checkout('AAAAAAAA') == 330
+@pytest.mark.parametrize("product_skus, expected", [
+    ("A", 50),
+    ("AA", 100),
+    ("AAA", 130),
+    ("AAAA", 180),
+    ("AAAAA", 200),
+    ("AAAAAA", 250),
+    ("AAAAAAA", 300),
+    ("AAAAAAAA", 330),
+])
+def test_product_a(product_skus, expected):
+    assert checkout_solution.checkout(product_skus) == expected
 
-
-def test_one_e_returns_40():
-    assert checkout_solution.checkout('E') == 40
-
-def test_two_e_returns_80():
-    assert checkout_solution.checkout('EE') == 80
-
-def test_two_e_gets_one_b_free():
-    assert checkout_solution.checkout('EEB') == 80
-
-def test_two_e_gets_one_b_free_but_no_b_discount():
-    assert checkout_solution.checkout('EEBB') == 110
 
 @pytest.mark.parametrize("product_skus, expected", [
     ("E", 40),
@@ -69,3 +57,4 @@ def test_product_e(product_skus, expected):
 ])
 def test_product_f(product_skus, expected):
     assert checkout_solution.checkout(product_skus) == expected
+
