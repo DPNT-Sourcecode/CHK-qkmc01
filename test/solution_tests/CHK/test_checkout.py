50aa326f6c729cf266dfad0da24f79fee564434d
sbx_user1051 <sbx_user1051@169.254.75.93>
2019-11-17 18:50:08 +0000

    Sun Nov 17 18:50:08 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,3 +3,10 @@ from solutions.CHK import checkout_solution
 
 def test_checkout():
     assert checkout_solution.checkout('ABCD') == 115
+    # multiples
+    assert checkout_solution.checkout('ABCDBA') == 195
+    # discount
+    assert checkout_solution.checkout('AAA') == 130
+    assert checkout_solution.checkout('AAAA') == 180
+    assert checkout_solution.checkout('AAAAAA') == 260
+
